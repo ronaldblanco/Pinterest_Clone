82c524cfb95e1135964befe0f0c15b741e603751
Ronald Blanco <pnald.blanco@gmail.com>
2017-06-29 21:58:49 +0000

    resize images ready

--- app/controllers/publicController.client.js
+++ app/controllers/publicController.client.js
@@ -14,6 +14,13 @@
       if(w > 319 && h > 279) return 'grid-item--width2 grid-item--height2';
       else if(w > 479 && h > 299) return 'grid-item--width3 grid-item--height3';
       else if(w > 639 && h > 359) return 'grid-item--width4 grid-item--height4';
+      else if(w > 479) return 'grid-item--width3';
+      else if(w > 639) return 'grid-item--width4';
+      else if(w > 319) return 'grid-item--width2';
+      else if(h > 359) return 'grid-item--height4';
+      else if(h > 299) return 'grid-item--height3';
+      else if(h > 319) return 'grid-item--height2';
+      else return '';
    }
    
    
@@ -22,7 +29,9 @@
       //console.log(wins.images);
       grid.innerHTML = '';
       for(var a = 0; a < wins.length; a++){
-         grid.innerHTML = grid.innerHTML + '<div class="grid-item-public '+gridConfig(wins[a].info.width,wins[a].info.height)+'"><img src = "'+wins[a].image+'" class = "myimg img-rounded" onError="this.onerror=null;this.src=`/public/img/noimage.jpg`;"><p class="txt">'+wins[a].name+'</p><p class="txtuser">By '+wins[a].username+'</p> <img src = "'+wins[a].userimage+'" class = "publicuser img-rounded"></div>'
+         var size = gridConfig(wins[a].info.width,wins[a].info.height);
+         if(size == '') grid.innerHTML = grid.innerHTML + '<div class="grid-item-public"><img src = "'+wins[a].image+'" class = "myimg img-rounded" onError="this.onerror=null;this.src=`/public/img/noimage.jpg`;"><p class="txt">'+wins[a].name+'</p><div class="txtuser"><p>By '+wins[a].username+'</p></div> <img src = "'+wins[a].userimage+'" class = "publicuser img-rounded"></div>'
+         else grid.innerHTML = grid.innerHTML + '<div class="grid-item-public '+size+'"><img src = "'+wins[a].image+'" class = "myimg img-rounded" onError="this.onerror=null;this.src=`/public/img/noimage.jpg`;"><p class="txt">'+wins[a].name+'</p><p class="txtuserbig">By '+wins[a].username+'</p> <img src = "'+wins[a].userimage+'" class = "publicuserbig img-rounded"></div>'
       }
       
    }
--- app/controllers/winController.client.js
+++ app/controllers/winController.client.js
@@ -9,12 +9,28 @@
    var image = document.querySelector('#image');
    var apiUrl = appUrl + '/api/:id/wins';
 
+   function gridConfig(w,h){
+      if(w > 319 && h > 279) return 'grid-item--width2 grid-item--height2';
+      else if(w > 479 && h > 299) return 'grid-item--width3 grid-item--height3';
+      else if(w > 639 && h > 359) return 'grid-item--width4 grid-item--height4';
+      else if(w > 479) return 'grid-item--width3';
+      else if(w > 639) return 'grid-item--width4';
+      else if(w > 319) return 'grid-item--width2';
+      else if(h > 359) return 'grid-item--height4';
+      else if(h > 299) return 'grid-item--height3';
+      else if(h > 319) return 'grid-item--height2';
+      else return '';
+   }
+   
    function updateGrid (data) {
       var wins = JSON.parse(data);
       console.log(wins.images);
       grid.innerHTML = '';
       for(var a = 0; a < wins.images.length; a++){
-         grid.innerHTML = grid.innerHTML + '<div class="grid-item"><input type="radio" value="?name=' +wins.images[a].name+'&image='+wins.images[a].image+ '" name="radioWins" id="radioWin'+a+'">This Win.<img src = "'+wins.images[a].image+'" class = "myimg img-rounded" onError="this.onerror=null;this.src=`/public/img/noimage.jpg`;"><p class="txt">'+wins.images[a].name+'</p><p class="txtuser">By '+wins.images[a].username+'</p>  <input type="radio" value="?name=' +wins.images[a].name+'&username='+wins.images[a].username+ '" name="radioLike" id="radioLike'+a+'">Likes '+wins.images[a].likes.length+'  <img src = "'+wins.images[a].userimage+'" class = "user img-rounded"></div>'
+         var size = gridConfig(wins.images[a].info.width,wins.images[a].info.height);
+         if(size == '') grid.innerHTML = grid.innerHTML + '<div class="grid-item"><input type="radio" value="?name=' +wins.images[a].name+'&image='+wins.images[a].image+ '" name="radioWins" id="radioWin'+a+'">This Win.<img src = "'+wins.images[a].image+'" class = "myimg img-rounded" onError="this.onerror=null;this.src=`/public/img/noimage.jpg`;"><p class="txt">'+wins.images[a].name+'</p><p class="txtuser">By '+wins.images[a].username+'</p>  <input type="radio" value="?name=' +wins.images[a].name+'&username='+wins.images[a].username+ '" name="radioLike" id="radioLike'+a+'">Likes '+wins.images[a].likes.length+'  <img src = "'+wins.images[a].userimage+'" class = "user img-rounded"></div>'
+         else grid.innerHTML = grid.innerHTML + '<div class="grid-item '+size+'"><input type="radio" value="?name=' +wins.images[a].name+'&image='+wins.images[a].image+ '" name="radioWins" id="radioWin'+a+'">This Win.<img src = "'+wins.images[a].image+'" class = "myimg img-rounded" onError="this.onerror=null;this.src=`/public/img/noimage.jpg`;"><p class="txt">'+wins.images[a].name+'</p><p class="txtuserbig">By '+wins.images[a].username+'</p>  <input type="radio" value="?name=' +wins.images[a].name+'&username='+wins.images[a].username+ '" name="radioLike" id="radioLike'+a+'">Likes '+wins.images[a].likes.length+'  <img src = "'+wins.images[a].userimage+'" class = "userbig img-rounded"></div>'
+         
       }
       
    }
